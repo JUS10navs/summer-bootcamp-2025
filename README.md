9d97b4e3f87ec0b0c91389b3ca69fe74a79cf7f2
Justine Navarro <justinehidalgo212@email.com>
2025-06-13 13:55:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+ # Github Version Control
+
+## Contact
+For any inquires, reach out via:
+### Email: johnny@gmail.com
---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import flask, render_template
+
+app = flask(__name__)
+
+app.route('/')
+def index():
+    return render_template("index.html")
+
+app.route('/')
+def index():
+    return render_template("login.html")
