648930c0510529f0ac61166a6e4765ac0f12024e
Justine Navarro <justinehidalgo212@email.com>
2025-06-13 14:10:49 +0800

    second commit

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app.route('/')
 def index():
     return render_template("index.html")
 
-app.route('/')
+app.route('/login')
 def index():
     return render_template("login.html")
+
+app.route('/register')
+def index():
+    return render_template("register.html")
